630c4fdc0238e99983b19ba1fc877f53f347c329
StickBrush [zerø] <StickBrush@users.noreply.github.com>
2019-11-15 20:00:23 +0100

    Changed to get port from env

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
 import connexion
+import os
 
 from swagger_server import encoder
 
 
 def main():
+    port = int(os.environ.get("PORT", 8080))
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Dámaso sensorización API'}, pythonic_params=True)
-    app.run(port=80)
+    app.run(port=port)
 
 
 if __name__ == '__main__':
