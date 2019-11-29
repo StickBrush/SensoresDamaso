ecf26d63cb51fb66f5d7b024cd4aca6f56ee44b3
StickBrush [zerø] <StickBrush@users.noreply.github.com>
2019-11-29 20:58:13 +0100

    Uptimer is now launched

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -4,10 +4,12 @@ import connexion
 import os
 
 from swagger_server import encoder
+from swagger_server.metrics.utils import Uptimer
 
 
 def main():
     port = int(os.environ.get("PORT", 8080))
+    uptimer = Uptimer()
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Dámaso sensorización API'}, pythonic_params=True)
